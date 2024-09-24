b29251351406504f9a57f56fec2166c5967221c7
Sajitha2004 <146451161+Sajitha2004@users.noreply.github.com>
2024-09-24 09:28:22 +0530

    Add files via upload

---
+++ DataTypes/src/org/datatypes/company.java
@@ -0,0 +1,17 @@
+package org.datatypes;
+
+public class company {
+
+	public static void main(String[]args) {
+		String name="sajitha";
+		byte age=21;
+	    long mobileNum = 123456789012345678l;
+		
+		System.out.println("The employee name is :"+ name +"\n" +
+				           "The employee age is : "+ age +"\n" +
+				           "The employee number  is :"+  mobileNum );
+		
+	}
+	
+
+}
