b3be395c6ca127dccab970f7264c0a19876ffce0
Sajitha2004 <146451161+Sajitha2004@users.noreply.github.com>
2024-09-24 09:32:52 +0530

    Add files via upload

---
+++ GreensTech/src/org/greenstech/GreensTech.java
@@ -0,0 +1,30 @@
+package org.greenstech;
+
+public class GreensTech {
+
+	private void greenOmr() {
+		System.out.println("Omr");
+	}
+	private void greenAdayar() {
+		System.out.println("Adayar");
+	}
+	private void greenTambaram() {
+		System.out.println("Tambaram");
+	}
+	private void greenVelacherry() {
+		System.out.println("Velacherry");
+	}
+	private void greenAnnaNagar() {
+		System.out.println("AnnaNagar");
+	}
+	
+	public static void main(String[] args) {
+		GreensTech a = new GreensTech();
+		a.greenOmr();
+		a.greenAdayar();
+		a.greenTambaram();
+		a.greenVelacherry();
+		a.greenAnnaNagar();
+	}
+
+}
