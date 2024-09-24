e54e5e06f4f3f8beca09b1c3dece4efd0df257d4
Sajitha2004 <146451161+Sajitha2004@users.noreply.github.com>
2024-09-24 09:33:35 +0530

    Add files via upload

---
+++ oddeven/src/org/oddeven/oddeven.java
@@ -0,0 +1,13 @@
+package org.oddeven;
+import java.util.Scanner;
+public class oddeven {
+
+	public static void main (String[]arg) {
+	Scanner = new Scanner(System.in);
+	int n = scan.nextInt();
+	int n= 10;
+	if(n%2 == 0) {
+		System.out.println("enter the  even number")
+	}
+	}
+}
