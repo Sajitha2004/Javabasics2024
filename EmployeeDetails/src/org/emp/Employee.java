9c3f927a2d3be1689e9d9333b610d0565a6dcde2
Sajitha2004 <146451161+Sajitha2004@users.noreply.github.com>
2024-09-24 09:31:42 +0530

    Add files via upload

---
+++ EmployeeDetails/src/org/emp/Employee.java
@@ -0,0 +1,36 @@
+package org.emp;
+
+public class Employee {
+
+	
+		// TODO Auto-generated constructor stub
+		private void empId() {
+			System.out.println("001");
+		}
+		private void empName() {
+			System.out.println("xxx");
+		}
+		private void empDob() {
+			System.out.println("1.1.2001");
+		}
+		private void empPhone() {
+			System.out.println("1236547890");
+		}
+		private void empEmail() {
+			System.out.println("xyz@gmailcom");
+		}
+		private void empAddress() {
+			System.out.println("4/352");
+		}
+		public static void main(String[] args) {
+			Employee a = new Employee();
+			a.empId();
+			a.empName();
+			a.empDob();
+			a.empPhone();
+			a.empEmail();
+			a.empAddress();
+		}
+	}
+
+
