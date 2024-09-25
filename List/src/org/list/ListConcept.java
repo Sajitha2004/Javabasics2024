3d405bc3a642bd15549f69f4433bc3b26126bfc7
Sajitha2004 <146451161+Sajitha2004@users.noreply.github.com>
2024-09-25 09:21:30 +0530

    Add files via upload

---
+++ List/src/org/list/ListConcept.java
@@ -0,0 +1,26 @@
+package org.list;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListConcept {
+	
+  public static void main(String[] args) {
+	List li = new ArrayList();
+	li.add(10);
+	li.add("saji");
+	li.add(false):
+	li.add(22.2);
+	li.add("n");
+	//for (int i=0;i<=4;i++) 
+	//{
+		//Object a = li.get(1);
+		//System.out.println(a);
+		
+	//}
+	
+	System.out.println(li);
+    }
+ 
+
+}
+
---
+++ ListMethod/src/org/listmethod/ListMethod.java
@@ -0,0 +1,15 @@
+package org.listmethod;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListMethod {
+	public static void main(String[] args) {
+		List li = new ArrayList();
+		li.add("saji");
+		li.add(12.22);
+		li.add(true);
+		System.out.println(li);
+	}
+
+	
+}
